2f428dfb02f653cbfc3fc1ba1d5ffdfc2265136c
Adam Romanek <romanek.adam@gmail.com>
2025-10-29 21:57:01 +0100

    Apply pre-commit on all files

--- manage.py
+++ manage.py
@@ -13,8 +13,8 @@ def main():
     try:
         from django.core.management import execute_from_command_line  # noqa: PLC0415
     except ImportError as exc:
-        raise ImportError(  # noqa: TRY003
-            "Couldn't import Django. Are you sure it's installed and "  # noqa: EM101
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?",
         ) from exc
